b50e94efa123c0135748e5ae1a013c1207db39c4
LoveJessyChen <48858531+LoveJessyChen@users.noreply.github.com>
2025-04-04 12:32:45 +0800

    5.1

--- devicemanagement/data_singleton.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from devicemanagement.constants import Device, Tweak
-
-class DataSingleton:
-    def __init__(self):
-        self.current_device: Device
-        self.device_available: bool = False
-        self.gestalt_path = None
